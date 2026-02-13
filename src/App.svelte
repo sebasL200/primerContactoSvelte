7ee23ac62ba1af82be9715bdd7816a7c57f1b44e
cono200 <tatattian04@gmail.com>
2026-02-13 12:43:24 -0700

    Ya fue mucho por hoy

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,18 @@
-<button on:click={() => console.log("Me han dado click")}>
-    Click here
-</button>
+<script>
+    let nombre = "";
+
+    function handle(e) {
+        const target = e.target;
+        nombre = e.target.value;
+        console.log(nombre);
+    }
+</script>
+
+
+<input type="text" placeholder="Pon aqui tu nombre" on:keyup={handle}>
+
+{#if nombre.length===0}
+<p>El campo nombre no puede estar vacio</p>
+{:else}
+<p>un gusto conocerte {nombre}</p>
+{/if}
